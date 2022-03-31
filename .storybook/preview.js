ea3b56e0096a4deb9c2c48036ecf958145151b19
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2022-03-31 16:46:52 +0900

    enable concurrent mode on storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,30 @@
+const ReactDOM = require('react-dom');
+const ReactDOMClient = require('react-dom/client');
+const nodes = new Map();
 
 export const parameters = {
   layout: "padded",
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// HACK: this will enable the concurrent mode of ReactDOM
+ReactDOM.render = (app, rootNode) => {
+	let root = nodes.get(rootNode);
+	if (!root) {
+		root = ReactDOMClient.createRoot(rootNode); // depending on your react version this might be `.createRoot`
+		nodes.set(rootNode, root);
+	}
+  console.debug('preview.js: now rendering with concurrent mode')
+	root.render(app);
+};
+
+ReactDOM.unmountComponentAtNode = (component) => {
+	const root = nodes.get(component);
+	if (root) {
+		root.unmount();
+		return true;
+	} else {
+		console.error("ReactDOM injection: can't unmount the given component");
+		return false;
+	}
+};
