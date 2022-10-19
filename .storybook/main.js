fece5770a73ccfe20739e335dd2a98d527a6b4d7
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2022-10-19 14:17:22 +0900

    wip: use swc instead of babel

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,19 @@ module.exports = {
   core: {
     builder: "webpack5",
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "storybook-addon-swc",
+      options: {
+        enable: true,
+        enableSwcLoader: true,
+        enableSwcMinify: true,
+      },
+    },
+  ],
   reactOptions: {
     fastRefresh: true,
     strictMode: true,
